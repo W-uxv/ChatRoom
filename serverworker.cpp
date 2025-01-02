489946b030fc3358461cd2786d897cb755dd7b0b
W-uxv <1901936667@qq.com>
2025-01-02 23:56:26 +0800

    finish desigend

--- serverworker.cpp
+++ serverworker.cpp
@@ -8,11 +8,22 @@ ServerWorker::ServerWorker(QObject *parent)
 {
     m_serverSocket = new QTcpSocket(this);
     connect(m_serverSocket, &QTcpSocket::readyRead, this, &ServerWorker::onReadyRead);
+    connect(m_serverSocket, &QTcpSocket::disconnected, this, &ServerWorker::disconnectedFromClient);
 }
 
 bool ServerWorker::setSocketDescriptor(qintptr socketDescriptor)
 {
     return m_serverSocket->setSocketDescriptor(socketDescriptor);
+}
+
+QString ServerWorker::userName()
+{
+    return m_userName;
+}
+
+void ServerWorker::setUserName(QString user)
+{
+    m_userName = user;
 }
 
 void ServerWorker::onReadyRead()
@@ -25,8 +36,16 @@ void ServerWorker::onReadyRead()
         socketStream.startTransaction();
         socketStream>>jsonData;
         if(socketStream.commitTransaction()) {
-            emit logMessage(QString::fromUtf8(jsonData));
-            sendMessage("I recieved message");
+            // emit logMessage(QString::fromUtf8(jsonData));
+            // sendMessage("I recieved message");
+
+            QJsonParseError parseError;
+            const QJsonDocument jsonDoc = QJsonDocument::fromJson(jsonData, &parseError);
+            if(parseError.error == QJsonParseError::NoError)
+            if(jsonDoc.isObject()) { // and is a JsoN object
+                emit logMessage(QJsonDocument(jsonDoc).toJson(QJsonDocument::Compact));
+                emit jsonReceived(this,jsonDoc.object()); // parse the JsoN
+            }
         }
         else break;
     }
@@ -47,4 +66,14 @@ void ServerWorker::sendMessage(const QString &text, const QString &type)
         // send the JSoN using
         serverStream<<QJsonDocument(message).toJson();
     }
+}
+
+void ServerWorker::sendJson(const QJsonObject &json)
+{
+    const QByteArray jsonData = QJsonDocument(json).toJson(QJsonDocument::Compact);
+    emit logMessage(QLatin1String("Sending to ") + userName() + QLatin1String(" - ") +
+                    QString::fromUtf8(jsonData));
+    QDataStream socketStream(m_serverSocket);
+    socketStream.setVersion(QDataStream::Qt_5_7);
+    socketStream << jsonData;
 }
